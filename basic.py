eb55d648a58b75c53f0a4e6970e4640420612c7f
lenapy <rashchyk1351@gmail.com>
2017-11-17 16:07:17 +0200

    learned  how to send POST request.

--- basic.py
+++ basic.py
@@ -1,5 +1,16 @@
 import urllib.request
+import urllib.parse
 
 """ get request """
 x = urllib.request.urlopen('https://www.google.com/')
-print(x.read())
+# print(x.read())
+
+""" post request, if we need to put some date to request, for example some search request """
+url = 'http://www.pythonprogramming.net'
+values = {'s': 'basic', 'submit': 'search'}
+data = urllib.parse.urlencode(values)
+data_enc = data.encode('utf-8')
+req = urllib.request.Request(url, data_enc)
+resp = urllib.request.urlopen(req)
+resp_data = resp.read()
+print(resp_data)
