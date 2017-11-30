f6046dccf5d76cb424be7ecf03c02dfee982ba2e
lenapy <rashchyk1351@gmail.com>
2017-11-30 16:05:50 +0200

    changed parser in amper_parse

--- amper_parse.py
+++ amper_parse.py
@@ -14,19 +14,38 @@ Date, Author, Title
 
 import urllib.request
 import re
-
-url = 'https://www.ampereanalysis.com/'
-response = urllib.request.urlopen(url)
-resp_data = response.read()
-authors = re.findall(r'<b>(.*?)</b>', str(resp_data))
-titles = re.findall(r'<u>(.*?)</u>', str(resp_data))
-mess_dates = re.findall(r'</b></a>(.*?)</span>', str(resp_data))
-clean_dates = re.findall(r'\d{1,3}/\d{1,3}/\d{1,3}', str(mess_dates))
+from settings import site_url
 
 
-parse_data = zip(clean_dates, authors, titles)
-save_data = open('parse_data.txt', 'w')
-for data in parse_data:
-    d = ','.join(data)
-    save_data.write(d)
-save_data.close()
+def get_response_data(url):
+    response = urllib.request.urlopen(url)
+    resp_data = response.read()
+    return resp_data
+
+
+def get_clean_data(resp_data):
+    authors = re.findall(r'<b>(.*?)</b>', str(resp_data))
+    titles = re.findall(r'<u>(.*?)</u>', str(resp_data))
+    mess_dates = re.findall(r'</b></a>(.*?)</span>', str(resp_data))
+    clean_dates = re.findall(r'\d{1,3}/\d{1,3}/\d{1,3}', str(mess_dates))
+    clean_data = zip(clean_dates, authors, titles)
+    return clean_data
+
+
+def save_amper_data(amper_data):
+    save_data = open('t.txt', 'a')
+    for data in amper_data:
+        d = ', '.join(data)
+        save_data.write(d)
+    save_data.close()
+
+
+if __name__ == '__main__':
+    for n in range(1, 15):
+        new_url = site_url + 'page' + str(n)
+        amper_response = get_response_data(new_url)
+        amper_data = get_clean_data(amper_response)
+        save_amper_data(amper_data)
+
+    with open('t.txt', 'r') as f:
+        print(f.readlines())
